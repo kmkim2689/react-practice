ce731a9537a16efdc5570e53fe1c66340c048cf6
Kim Gimun <kmkim7575@gmail.com>
2023-04-30 23:31:21 +0900

    [FEAT] 영화 아이템 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,107 +1,95 @@
-import { useState, useEffect } from 'react';
-import styles from './App.module.css';
-import Button from './Button';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  // const [counter, setCounter] = useState(0);
-  // const [keyword, setKeyword] = useState("");
-  // const onChange = (event) => {
-  //   setKeyword(event.target.value);
-  // }
-  // const onClick = () => {
-  //   setCounter((prev) => prev + 1);
-  // }
-  // // 컴포넌트가 처음으로 render될 때, console에 rendered가 찍힘
-  // // state 변수에 변화가 생기면, 당연히 해당 컴포넌트 전체 차원에서 재렌더링이 일어나기 때문에 rendered가 또 찍힘.
-  // // 여기서 문제점 : 
-  // // 다시 render될 때마다 반복실행되어도 괜찮은 코드가 있는 반면, 하지만, 그렇게 하지 않고 component가 처음 렌더링 될 때에만 컴포넌트의 코드가 실행되도록 해야 할 때가 있음. 즉 첫번째 렌더링 때에만 코드가 실행되도록 해야 할 때가 있음..
-  // // 예를 들어, API를 통해 데이터를 가져올 때, 처음 렌더링 할 때에만 API를 호출하도록 하는 것이 훨씬 효율적일 것임. 
-  // // 이것을 effect를 통해 해결할 수 있음.
-  // // useEffect : 2개의 인자를 가지는 function
-  // // 첫 번째 인자 : 컴포넌트가 처음 렌더링될 때 딱 한번만 실행하고 싶은 코드(화살표 함수를 직접 넣어도 되고, 함수명을 넣어도 되는 등)
-  // // 두 번째 인자 : dependencies 배열이 들어감. 배열 안에는 변화를 감지할 변수들을 넣는다. 그리고 그 변수들에 대한 변화가 일어날 때 첫번째 인자에 넣은 함수가 실행되는 것이다. 참고로, 빈 배열이 주어지면, 그 코드는 처음 렌더링될 때 한번만 실행됨을 의미한다. 지켜볼 것이 없기ㅣ 때문
-
-  // // 만약, 상단에 검색창을 두기 위해 input(type text)을 두고, state변수를 검색창에 들어가는 글자로 지정한다면, onChange를 설정함으로써 글자를 추가하거나 삭제할 때마다 state의 변경이 일어나기 때문에 코드 재실행 및 렌더링이 수도 없이 일어나게 된다. 이는 매우 비효율적이다. 
-
-  // // 다른 문제점으로는, keyword 이외에 다른 state변수(counter)가 변화할 때에도 검색이 다시 이뤄진다는 것이다. 이러한 관심사 분리를 위하여 effect 함수가 필요한 것이다.
-
-  // // 코드의 특정한 부분만이 변화했을 때, 원하는 코드들만 실행되도록 하는 방법이 필요함
-
-  // console.log('rendered');
-  // useEffect(() => {
-  //   console.log("call the api")
-  // }, []);
-
-  // // keyword가 변화할 때에만 함수를 실행하도록 하는 effect
-  // useEffect(() => {
-  //   // 처음 렌더링 될 때에는 검색을 하지 않도록 하기 위해 조건을 걸어줌
-  //   if (keyword !== "" && keyword.length > 5) {
-  //     console.log("search for", keyword);
-  //   }
-  // }, [keyword]);
-
-  // useEffect(() => {
-  //   console.log("counter changed", counter);
-  // }, [counter]);
-
-  // useEffect(() => {
-  //   console.log("keyword or counter changed");
-  // }, [counter, keyword]);
-  
-  // return (
-  //   <div>
-  //     <input type="text" placeholder='search here' value={keyword} onChange={onChange} />
-  //     <h1>
-  //       {counter}
-  //     </h1>
-  //     <button onClick={onClick}>click me</button>
-  //   </div>
-  // );
-
-  // cleanup function
-  // 일례로, 검사에서 element 메뉴에서 태그들을 보면, 버튼을 누를 때마다 구성되는 태그들이 변경되는 것을 알 수 있다. 특히나, h1 태그가 버튼을 클릭함에 따라 dom에서 사라지기도 하고(hide) 다시 생기기도(show) 한다. => destroy. 아예 코드에도 남아있지 않고 사라짐.
-  // 특정 component가 destroy될 때에도 원하는 코드를 실행할 수 있음
-  
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
   return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
-  );
-  
+  <Router>
+    <Routes>
+      <Route path="/hello" element={<h1>hello</h1>}>
+      </Route>
+      <Route path="/" element={<Home />}>
+        {/* 여기에 컴포넌트를 작성한다. 
+        보통, path가 "/"라면 홈화면을 의미
+      element 속성에는 띄워줄 component를 import하기만 하면 끝. 앱을 실행해보면, 도메인 뒤에 "/"만 붙어있음을 알 수 있다. 이렇게 함으로써, url마다 정확히 원하는 컴포넌트를 보여줄 수 있게 된다.*/}
+      </Route>
+      <Route path="/movie/:id" element={<Detail />}>
+      </Route>
+    </Routes>
+  </Router>
+  )
 }
-
-function Hello() {
-  function byeFunc() {
-    console.log("bye");
-  }
-
-  function hiFunc() {
-    console.log("hi");
-    return byeFunc;
-  }
-
-  useEffect(hiFunc, []);
-
-  // useEffect(() => {
-  //   console.log("component created!")
-  //   // useEffect 첫번째 인자 함수 내부에서 return문으로 반환하는 함수 : component가 destroy되면서 실행되는 코드 => cleanup function!!
-  //   // 이것을 통하여, component가 언제 create되었는지, 언제 destroy되었는지 알 수 있음...
-  //   return () => console.log("component destroyed!");
-  // }, []);
-
-  return (
-    <h1>hello</h1>
-  );
-}
-
-// 버튼 클릭시, created :) , destroyed :(, created :) 메세지가 3개가 등장하였습니다.
-// 이유는 index.js의 React.StrictMode 가 활성화 되어 있어서네요.
-// 급하게 강의를 따라하느라 끄라고 했었는지는 기억이 잘 나지 않지만 혹시 저와 같이 함수가 두번 실행되는 경우 (double invoke)가 발생하면 위의 코드를 주석처리 하시면 될것 같습니다.
-
-// strictmode의 경우, 개발 과정중 안전을 위해 켜는 기능이며 배포시에 자동으로 해제된다고 합니다.
-
 
 export default App;
+
+/*
+react.js 앱에서 페이지를 전환하는 방법
+화면의 어느 요소를 눌렀을 때, 다른 페이지로 넘어가는 방법
+
+map을 이용하여, 배열 안의 여러 요소들을 순회하여 각 요소들을 렌더링하려고 할 때, 반드시 key 속성을 넣어주어야 한다. key는 각각의 아이템마다 유일해야 한다.
+
+React Router란?
+페이지를 전환하기 위하여 사용되는 것.
+
+route : 각 하나의 화면, url 혹은 페이지와 대응된다.
+ex)
+home route : 모든 영화에 대한 개괄을 보여줌
+movie route : 각 영화에 대한 자세한 정보를 보여줌
+
+사용 방법
+https://reactrouter.com/en/6.11.0/start/tutorial
+1. 터미널에 다음과 같이 입력
+npm install react-router-dom
+
+2. 화면들을 담기 위하여 아래와 같이 2개의 폴더를 마련한다.
+- routes -> 하위에 Home.js 파일(컴포넌트)을 만든다. 이는 홈화면 하나를 위한 파일이다. -> 여태 App.js에서 작업한 것들을 모두 그곳으로 옮긴다.
+그리고, 영화에 대한 상세정보를 보여주는 화면을 만들기 위하여 Detail.js 라우터를 만든다.
+- components -> 하위에 Movie.js 컴포넌트를 만든다.
+
+3. 이러한 과정을 거치면, App.js에는 아무것도 남지 않게 된다.(return null;)
+이제 App.js가 하게 되는 역할은 router를 render하는 것이다. 
+
+url에 따라서, 각각의 다른 컴포넌트를 보여주기 위하여,
+4. route를 위해 필요한 것들을 import한다.
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+참고로, BrowserRouter와 HashRouter가 있는데, BrowserRouter는 우리가 생각하는 그대로의 url을 나타내고, HashRouter는 뒤에 #이 붙는다는 것에서 차이가 있다.
+
+5. 형태
+<Router>
+  <Routes>
+    <Route path="url" element={<컴포넌트명 />}>
+    </Route>
+  </Routes>
+</Router>
+
+6. 한 Route에서 다른 Route로 이동하는 방법
+- 영화 제목 클릭 시, Detail 스크린으로 이동
+- HTML에서 a 태그 이용 가능
+예를 들어, Movie 컴포넌트의 제목 h2 태그를 클릭하면 이동하도록 하기 위해 <h2><a href="/movie"></a></h2> 이런 식으로
+- a태그를 사용하는 것의 문제점 : 페이지 전체가 다시 실행됨 => 이는 React의 장점을 십분 활용하지 못하는 방법이다.
+- 이 때, "Link"라는 컴포넌트를 사용한다.
+- Link : 브라우저 새로고침 없이 다른 페이지로 이동할 수 있도록 하는 컴포넌트
+- Link의 to 속성을 이용. to 속성에 이동할 url을 작성
+
+6. 동적 url 만들기 - parameters
+'동적' : url에 변수를 넣을 수 있다는 의미
+ex) /movie/123
+여기서 123은 영화의 넘버를 나타내는 변수를 의미
+
+사용법 : route 컴포넌트에서 ":변수명" 명시
+ex) /user/:id
+=> /user/10 : 아이디가 10번인 user
+그 후, 컴포넌트에서 Link 태그의 to 속성에 
+식별자 변수를 넣는다.`${변수명}`
+
+7. useParams 사용(Detail 컴포넌트 참고)
+*/
